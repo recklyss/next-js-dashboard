5eab85d4e1f6f7f2fa59554737b3bb6d672e5e95
Recklyss <zhangjiahengpoping@gmail.com>
2024-06-02 00:49:17 +0700

    feat: finish the first 3 courses of next-js

---
+++ app/ui/fonts.ts
@@ -0,0 +1,4 @@
+import { Schoolbell, Lusitana } from 'next/font/google';
+
+export const schoolbell = Schoolbell({weight: "400", subsets: ['latin']});
+export const lusitana = Lusitana({weight: "400", subsets: ['latin']});
